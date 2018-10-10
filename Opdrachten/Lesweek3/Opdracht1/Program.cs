b07b3e00aa4053d8723347198d5d0f435c6ed038
Owen De Waele <owendewa@student.arteveldehs.be>
2018-10-10 14:57:05 +0200

    Validatie op tinder v1

--- Opdrachten/Lesweek3/Opdracht1/Program.cs
+++ Opdrachten/Lesweek3/Opdracht1/Program.cs
@@ -7,6 +7,20 @@ namespace Opdracht1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+           int a = 5;
+           int b = 6;
+           int resultaat1 =  optellen(a , b);
+           int resultaat2 = deling(a , b);
+           int resultaat3 = verhogen(a);
+        }
+        static int optellen(int a , int b){
+            return a + b; 
+        }
+        static int deling(int a , int b){
+            return a / b; 
+        }
+        static int verhogen(int a){
+            return a++ ; 
         }
     }
 }
