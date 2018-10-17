2968a1d406d7779c6f3bb31c2572ab9b1767ff87
Owen De Waele <owendewa@student.arteveldehs.be>
2018-10-17 14:35:55 +0200

    Apps

---
+++ Opdrachten/Lesweek4/Opdracht2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Opdracht2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            for(int x = 0; x< 11; x++){
+                for(int y = 0; y<11; y++){
+                    Console.WriteLine(x+" maal "+y+" is gelijk aan "+ x*y);   
+                }
+            }
+            
+        }
+    }
+}
