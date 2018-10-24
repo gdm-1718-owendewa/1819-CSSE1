34676b14b46bf94c2e6202cd53dbcaa7609a8a8d
Owen De Waele <owendewa@student.arteveldehs.be>
2018-10-24 11:03:08 +0200

    Lesweek 5 remove speces

--- Opdrachten/Lesweek4/Opdracht2/Program.cs
+++ Opdrachten/Lesweek4/Opdracht2/Program.cs
@@ -8,28 +8,34 @@ namespace Opdracht2
         {
             Console.WriteLine("Hello World!");
 
-            for(int x = 0; x< 11; x++){
-                for(int y = 0; y<11; y++){
+            int grondgetal = 41;
+            int vermeningvuldigingsFactor = 21;
+            for(int x = 0; x< grondgetal; x++){
+                for(int y = 0; y<vermeningvuldigingsFactor; y++){
                     Console.WriteLine(x+" maal "+y+" is gelijk aan "+ x*y);   
                 }
             }
-            for(int i=0; i<10; i++){
+           for(int i=0; i<20; i++){
             Console.WriteLine(Fo(i));
              }
             
             //fac van een nummer//
-            int a, number, fac;
+            Fac();             
+
+              
+           
+        }
+        static void Fac(){
+              int a, number, fac;
             Console.WriteLine("enter your number");
             number = int.Parse(Console.ReadLine());
             fac = number;
-            for(a=number-1; a >=1; a--){
-                fac = fac * a;
-            }
+                for(a=number-1; a >=1; a--){
+                    fac = fac * a;
+                }
             Console.WriteLine("Factoriaal van ingevoerd nummer is "+ fac);
             Console.ReadLine();
-             
         }
-        
         static int Fo(int number){
             int eerste = 0;
             int tweede = 1;
---
+++ Opdrachten/Leweek5/opdarcht1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace opdarcht1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string test = "dit is een test";
+            string[] woorden = test.Split(' ');
+            foreach(var woord in woorden ){
+                Console.WriteLine(woord);
+            }
+            char[] spatie = {' '};
+        }
+    }
+}
