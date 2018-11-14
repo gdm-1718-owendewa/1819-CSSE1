d53b5d7cc9c4a523fa6f3c365aabefe13b88a20d
Owen De Waele <owendewa@student.arteveldehs.be>
2018-11-14 18:32:34 +0100

    Added auteur check

--- Opdrachten/Lesweek7/Opdracht2/Program.cs
+++ Opdrachten/Lesweek7/Opdracht2/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using System.Collections;
 namespace Opdracht2
 {
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Voertuig voertuig1 = new Voertuig();
+            voertuig1.Laadvermogen = 12;
+            voertuig1.Kleur = "blauw";
+            Console.WriteLine(voertuig1.ToString());
+
+            Voertuig voertuig2 = new Voertuig(30, "rood");
+            voertuig2.ToString();
+
+            Auto auto1 = new Auto();
+            Console.WriteLine(auto1.ToString());
         }
+
     }
 }
---
+++ Opdrachten/Lesweek7/Opdracht2/auto.cs
@@ -0,0 +1,14 @@
+namespace Opdracht2
+{
+class Auto : Voertuig
+{
+    private int aantalDeuren;
+    private int aantalZitPlaatsen;
+    private bool trekhaak;
+    private int aantalWielen;
+
+    public Auto(){
+        this.Laadvermogen = 20;
+    }
+}
+}
