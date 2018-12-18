a43be3f81ab99a6e6fdab027a15ca41b32b5389d
Owen De Waele <owendewa@student.arteveldehs.be>
2018-12-18 19:26:36 +0100

    opdracht6

---
+++ Opdrachten/lesweek12/Opdracht 6/Deel 3/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Deel_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Geef uw figuur in Cirkel/Vierkant/Rechthoek");
+            string type = Console.ReadLine();
+            Vorm obj = CreateVorm.getobject(type);
+            Console.WriteLine(obj.getShape());
+            Console.ReadLine();
+            
+
+        }
+    }
+    public class CreateVorm{
+        public static Vorm getobject(string typeofobj){
+            Vorm obj = null;
+            if(typeofobj.ToLower()=="cirkel"){
+                obj = new Cirkel();
+            }else if (typeofobj.ToLower()=="vierkant"){
+                obj = new Vierkant();
+            }else{   
+                obj = new Rechthoek();
+            }
+            return obj;
+        }
+    }
+   public interface Vorm{
+        string getShape();
+    }
+
+    class Cirkel: Vorm{
+        public virtual string getShape(){
+            return "Dit is een cirkel";
+        }
+    }
+    class Vierkant: Vorm{
+        public virtual string getShape(){
+            return "Dit is een vierkant";
+        }
+    }
+    class Rechthoek: Vorm{
+        public virtual string getShape(){
+            return "Dit is een rechthoek";
+        }
+    }
+    
+}
